a39c2f2ff79c0e3f6b6dc4131c3e305d5c0ccbe7
Bruno Menezes <brunodmenezes@gmail.com>
2015-01-11 17:48:47 -0200

    high-order functions, parecido com closure no groovy, evitando problema caso um bloco não seja passado com block_given?

--- loja_virtual/lib/biblioteca.rb
+++ loja_virtual/lib/biblioteca.rb
@@ -6,7 +6,18 @@ class Biblioteca
   end
 
   def adiciona(livro)
-    @livros[livro.categoria] ||=[]
+    @livros[livro.categoria] ||= []
     @livros[livro.categoria] << livro
   end
+
+  def livros
+    @livros.values.flatten
+  end
+
+  def livros_por_categoria(categoria)
+    @livros[categoria].each do |livro|
+      yield livro if block_given?
+    end
+  end
+
 end
--- loja_virtual/loja_app.rb
+++ loja_virtual/loja_app.rb
@@ -4,19 +4,9 @@ require 'set'
 teste_e_design = Livro.new "Mauricio Aniche", "123454", 247, 60.9, :testes
 web_design_responsivo = Livro.new "Tárcio Zemel", "123454", 189, 70.9, :web_design
 
-# biblioteca = Biblioteca.new
-#
-# biblioteca.adiciona teste_e_design
-# biblioteca.adiciona web_design_responsivo
-#
-# for categoria, livros in biblioteca.livros do
-#   p categoria
-#
-#   for livro in livros do
-#     p livro.valor
-#   end
-# end
+biblioteca = Biblioteca.new
 
-livros = Set.new [teste_e_design, web_design_responsivo]
+biblioteca.adiciona teste_e_design
+biblioteca.adiciona web_design_responsivo
 
-p livros
+biblioteca.livros_por_categoria :testes
